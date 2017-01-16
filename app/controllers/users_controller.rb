4d95fa1453b6a6585e5c61feccbe24849a9dbc70
Masato Ito <masarusan24@hotmail.com>
2017-01-16 10:37:49 +0000

    7.3.1 end

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,13 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      # 保存成功
+    else
+      render 'new'
+    end
+  end
 end
