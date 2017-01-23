f480b88c7ed09c3d2121c414e6db65cfad109161
masarusan24 <masarusan24@gmail.com>
2017-01-23 02:06:06 +0000

    7.3.4 end

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,22 +1,30 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-  
+
   # 登録失敗用のテスト
   test "invalid signup information" do
     # getメソッドを使ってユーザー登録ページにアクセス
-    get signup_path 
-    
+    get signup_path
+
     # assert_no_differenceのブロック(postメソッド)を実行する前後で
     # 引数の値 (User.count) が変わらないことをテストする
     assert_no_difference 'User.count' do
-      post users_path, params: { user: { name: "",  # Rails5.0からparamsハッシュを明示的に含めることが推奨されている
+      post signup_path, params: { user: { name: "",  # Rails5.0からparamsハッシュを明示的に含めることが推奨されている
                         email: "user@invalid",
                         password: "foo",
-                        password_confirmation: "bar" } }
+                        password_confirmation: "bar" }
+                      }
     end
-    
+
     # 送信に失敗してnewアクションが再描画されることをテスト
     assert_template 'users/new'
+
+    # エラーメッセージのテスト
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
+
+    # POST送信先が正しいかどうかのテスト
+    assert_select 'form[action="/signup"]'
   end
 end
