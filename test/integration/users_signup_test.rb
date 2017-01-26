a8e96ee9b07a14635b7773b280019dc0a7eaaad1
Masato Ito <masarusan24@hotmail.com>
2017-01-26 06:32:51 +0000

    7.4.4 end

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -27,4 +27,25 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     # POST送信先が正しいかどうかのテスト
     assert_select 'form[action="/signup"]'
   end
+  
+  # 登録成功用のテスト
+  test "valid signup information" do
+    # getメソッドを使ってユーザー登録ページにアクセス
+    get signup_path
+
+    # assert_differenceのブロック(postメソッド)を実行する前後で
+    # 引数の値 (User.count) が変わっている(1になっている)ことをテストする
+    assert_difference 'User.count', 1 do
+      post signup_path, params: { user: { name: "Example User",  # Rails5.0からparamsハッシュを明示的に含めることが推奨されている
+                        email: "user@valid.com",
+                        password: "password",
+                        password_confirmation: "password" }
+                      }
+    end
+
+    # 送信に成功してshowアクションが再描画されることをテスト
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.blank?
+  end
 end
