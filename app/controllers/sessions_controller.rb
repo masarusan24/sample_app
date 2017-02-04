c1a957cad201e127e959fd65a44875e54b2a8ef2
Masato Ito <masarusan24@hotmail.com>
2017-02-04 06:44:06 +0000

    8.1.4 end

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
       # ユーザログイン後にユーザ情報のページにリダイレクトする
     else
       # エラーメッセージを生成する
+      flash[:danger] = 'invalid email/password combination' # 間違い
       render 'new'
     end
   end
