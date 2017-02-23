a2336c6d7ac5d6cfff211a63355c3fdcf510648f
Masato Ito <masarusan24@hotmail.com>
2017-02-23 15:17:40 +0000

    9.3.1 end

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -39,4 +39,14 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert flash.empty? # フラッシュメッセージが空になっている
   end
   
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_not_nil cookies['remember_token']
+  end
+  
+  test "login without remembering" do
+    log_in_as(@user, remember_me: '0')
+    assert_nil cookies['remember_token']
+  end
+  
 end
