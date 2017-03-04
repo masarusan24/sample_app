b0fec1108ca28cb7c74374e9880ad25ffe5bd337
Masato Ito <masarusan24@hotmail.com>
2017-03-04 11:01:07 +0000

    10.2.1 end

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -8,6 +8,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   
   # 間違った情報で編集した時のテスト
   test "unsuccessful edit" do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     patch user_path(@user), params: { user: { name: "",
@@ -20,6 +21,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   
   # 正しい編集テスト
   test "successful edit" do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     name = "Foo Bar"
@@ -33,5 +35,20 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     @user.reload
     assert_equal name, @user.name
     assert_equal email, @user.email
+  end
+  
+  # 未ログイン状態で編集画面に行こうとした場合、ログインページに遷移するかどうかのテスト
+  test "should redirect edit when not logged in" do
+    get edit_user_path(@user)
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+  
+  # 未ログイン状態でユーザ情報を更新しようとした場合、ログインページに遷移するかどうかのテスト
+  test "should redirect update when not logged in" do
+    patch user_path(@user), params: { user: {name: @user.name,
+                                  email: @user.email } }
+    assert_not flash.empty?
+    assert_redirected_to login_url
   end
 end
