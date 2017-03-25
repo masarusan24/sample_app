f2a829aca1c20c20a2dac6f388b563d56b0690ea
Masato Ito <masarusan24@hotmail.com>
2017-03-25 06:23:20 +0000

    10.4.3 end

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -3,16 +3,32 @@ require 'test_helper'
 class UsersIndexTest < ActionDispatch::IntegrationTest
   
   def setup
-    @user = users(:masato)
+    @admin = users(:masato)
+    @non_admin = users(:kaori)
   end
   
-  test "index including pagination" do
-    log_in_as(@user)
+  # 管理者ログインした場合、ユーザ名と削除リンクが出現する(管理者には削除リンク出ない)
+  test "index as admin including paginations and delete links" do
+    log_in_as(@admin)
     get users_path
     assert_template 'users/index'
     assert_select 'div.pagination'
-    User.paginate(page: 1).each do |user|
+    first_page_of_users = User.paginate(page: 1)
+    first_page_of_users.each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
+      unless user == @admin
+        assert_select 'a[href=?]', user_path(user), text: 'delete'
+      end
+    end
+    assert_difference 'User.count', -1 do
+      delete user_path(@non_admin)
     end
   end
+  
+  test "index as non-admin" do
+    log_in_as(@non_admin)
+    get users_path
+    assert_select 'a', text: 'delete', count: 0
+  end
+  
 end
