ac84cf1ce86125d01413030861ff517a9c260622
Masato Ito <masarusan24@hotmail.com>
2017-04-09 15:00:53 +0000

    11.3.3 end

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
 
   # 登録失敗用のテスト
   test "invalid signup information" do
@@ -28,25 +32,38 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'form[action="/signup"]'
   end
   
-  # 登録成功用のテスト
-  test "valid signup information" do
+  # 登録成功用のテスト(アクティベーション)
+  test "valid signup information with account activation" do
     # getメソッドを使ってユーザー登録ページにアクセス
     get signup_path
 
     # assert_differenceのブロック(postメソッド)を実行する前後で
     # 引数の値 (User.count) が変わっている(1になっている)ことをテストする
     assert_difference 'User.count', 1 do
-      post signup_path, params: { user: { name: "Example User",  # Rails5.0からparamsハッシュを明示的に含めることが推奨されている
+      post users_path, params: { user: { name: "Example User",  # Rails5.0からparamsハッシュを明示的に含めることが推奨されている
                         email: "user@valid.com",
                         password: "password",
-                        password_confirmation: "password" }
-                      }
+                        password_confirmation: "password" } }
     end
-
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    user = assigns(:user)
+    assert_not user.activated?
+    # 有効化してない状態でログインしてみる
+    log_in_as(user)
+    assert_not is_logged_in?
+    # 有効化トークンが不正な場合
+    get edit_account_activation_path("invalid token", email: user.email)
+    assert_not is_logged_in?
+    # トークンは正しいがメールアドレスが無効な場合
+    get edit_account_activation_path(user.activation_token, email: 'wrong')
+    assert_not is_logged_in?
+    # 有効化トークンが正しい場合
+    get edit_account_activation_path(user.activation_token, email: user.email)
+    assert user.reload.activated?
     # 送信に成功してshowアクションが再描画されることをテスト
     follow_redirect!
-    #assert_template 'users/show'
+    assert_template 'users/show'
     #assert_not flash.blank?
-    #assert is_logged_in?
+    assert is_logged_in?
   end
 end
