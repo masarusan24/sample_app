a4aa413aae264508339acb8307d1d7c764324fa0
Masato Ito <masarusan24@hotmail.com>
2017-04-09 09:23:00 +0000

    11.3.1 end test success

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,7 +33,7 @@ class User < ApplicationRecord
   def authenticated?(attribute, token)
     digest = send("#{attribute}_digest")
     return false if digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    BCrypt::Password.new(digest).is_password?(token)
   end
   
   # ユーザのログイン情報を破棄する
