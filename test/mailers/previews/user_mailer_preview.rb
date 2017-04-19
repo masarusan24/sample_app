49446dc8303b0edfc8a9486cf3136017c2f07971
Masato Ito <masarusan24@hotmail.com>
2017-04-19 14:03:37 +0000

    12.2.1 end

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -3,15 +3,17 @@ class UserMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/account_activation
   def account_activation
-    #user = User.first
-    user = User.find_by(email: "masarusan24@hotmail.com")
+    user = User.first
+    #user = User.find_by(email: "masarusan24@hotmail.com")
     user.activation_token = User.new_token
     UserMailer.account_activation(user)
   end
 
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/password_reset
   def password_reset
-    UserMailer.password_reset
+    user = User.first
+    user.reset_token = User.new_token
+    UserMailer.password_reset(user)
   end
 
 end
