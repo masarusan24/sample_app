d690911ccbfefc6e265a58ca89951e2bc6a209db
Masato Ito <masarusan24@hotmail.com>
2017-04-23 09:47:14 +0000

    12.2.2 end

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -16,7 +16,7 @@ class UserMailerTest < ActionMailer::TestCase
 
   test "password_reset" do
     user = users(:masato)
-    user.activation_token = User.new_token
+    user.reset_token = User.new_token
     mail = UserMailer.password_reset(user)
     assert_equal "Password reset", mail.subject
     assert_equal [user.email], mail.to
